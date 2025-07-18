da247a12d06d2821b254b5852798c8d3fd7e50cb
EuNetu <euneto2501@gmail.com>
2025-07-18 02:58:18 -0300

    feat: Cria borda animada

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -3,32 +3,34 @@ import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
-import { useCycle, motion } from "motion/react";
+import { motion, useCycle } from "motion/react";
 
 export function UploadWidget() {
+	const isThereAnyPendindUploads = true;
 
-	const [isWidgetOpen, toggleWidgetOpen ] = useCycle(false, true);
+	const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
 	return (
-		<Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
-			<motion.div 
-				className="bg-zinc-900 overflow-hidden max-w-[360px] rounded-xl shadow-shape"
-				animate={isWidgetOpen ? 'open' : 'closed'}
+		<Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+			<motion.div
+				data-progress={isThereAnyPendindUploads}
+				className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape  data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+				animate={isWidgetOpen ? "open" : "closed"}
 				variants={{
 					closed: {
-						width: 'max-content',
+						width: "max-content",
 						height: 44,
 						transition: {
-							type: 'inertia',
-						}
+							type: "inertia",
+						},
 					},
 					open: {
 						width: 360,
-						height: 'auto',
+						height: "auto",
 						transition: {
-							duration: 0.1
-						}
-					}
+							duration: 0.1,
+						},
+					},
 				}}
 			>
 				{!isWidgetOpen && <UploadWidgetMinimizedButton />}
@@ -39,12 +41,12 @@ export function UploadWidget() {
 					<div className="flex flex-col gap-4 py-3">
 						<UploadWidgetDropzone />
 
-						<div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+						<div className="h-px bg-zinc-800 border-t border-black/50 box-content" />
 
 						<UploadWidgetUploadList />
 					</div>
 				</Collapsible.Content>
 			</motion.div>
 		</Collapsible.Root>
-	)
+	);
 }
